6f00c3fa0ae8140fe049f25a46cbb814495e8345
aryannnn07 <wankhdearyan07@gmail.com>
2024-12-29 12:05:27 +0530

    "Added 'test' to empty README file."

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+  const user = "Aryan"
+  return (
+    <>
+    <Chai />
+    <h2>hehehehe {user}</h2>
+    </>
+    
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,9 @@
+function Chai(){
+    return(
+        <>
+        <h2>Chai is ready</h2>
+        <h3>Chai is not good</h3>
+        </>
+    )
+}
+export default Chai;
